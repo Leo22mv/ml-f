1109c16a509ca00c2a7ab839addc5de41af21a8e
Kbza <g.leonel.m17@gmail.com>
2023-07-21 05:38:11 -0300

    0.34

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -7,7 +7,7 @@ import { Injectable } from '@angular/core';
 export class AuthService {
 
   // uri: string = "http://localhost:8080";
-  uri: string = "ml-b-s.azurewebsites.net";
+  uri: string = "https://ml-b-s.azurewebsites.net";
 
   constructor(private http: HttpClient) { }
 
