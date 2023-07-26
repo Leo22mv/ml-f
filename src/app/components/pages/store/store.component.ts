bd0f4959571639a32135466824adca01ef00fd67
Kbza <g.leonel.m17@gmail.com>
2023-07-26 17:51:55 -0300

    0.75

--- src/app/components/pages/store/store.component.ts
+++ src/app/components/pages/store/store.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-store',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StoreComponent implements OnInit {
 
-  constructor() { }
+  loading: boolean = true;
+  uri = "";
+
+  productList: any = [];
+
+  constructor(private http: HttpClient, private prodServ: ProductService) { }
 
   ngOnInit(): void {
+    this.uri = this.prodServ.uri
+    this.http.get<any[]>(this.uri + "/productos").subscribe(res => {
+      this.loading = false;
+      this.productList = res;
+    }, err => {
+      this.loading = false;
+    })
   }
 
 }
