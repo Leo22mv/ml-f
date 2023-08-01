cf9fd035d5c58391702666ef224ea66bdaebf611
Kbza <g.leonel.m17@gmail.com>
2023-08-01 09:55:33 -0300

    0.95

--- src/app/components/pages/store/store.component.ts
+++ src/app/components/pages/store/store.component.ts
@@ -62,16 +62,17 @@ export class StoreComponent implements OnInit {
   ngOnInit(): void {
     this.uri = this.prodServ.uri
     this.prodServ.getProducts().subscribe(res => {
+      let pr = res;
       for (let product of this.cartService.cart) {
-        for (let prod of res) {
+        for (let prod of pr) {
           if (product.id_product==prod.id_product) {
             prod.stock -= product.quantity;
           }
         }
       }
       this.loading = false;
-      this.productList = res;
-      this.totalProductList = res;
+      this.productList = pr;
+      this.totalProductList = pr;
     }, err => {
       this.loading = false;
     })
