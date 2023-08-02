7c4c9d46a633c2f9cb8d19972ccd861dafdd9a86
Kbza <g.leonel.m17@gmail.com>
2023-08-02 07:15:13 -0300

    0.95

--- src/app/components/pages/login/login.component.ts
+++ src/app/components/pages/login/login.component.ts
@@ -48,13 +48,13 @@ export class LoginComponent implements OnInit {
         this.btnClass = "";
         this.logged = true;
 
-        // this.auth.getUsers().subscribe(resp => {
-        //   for (let user of resp) {
-        //     if (user.username==this.username) {
-        //       localStorage.setItem("id", user.id_user);
-        //     }
-        //   }
-        // })
+        this.auth.getUsers().subscribe(resp => {
+          for (let user of resp) {
+            if (user.username==this.username) {
+              localStorage.setItem("id", user.id_user);
+            }
+          }
+        })
 
         localStorage.setItem("auth_token", res.token);
         localStorage.setItem("role", res.Role[0].authority);
