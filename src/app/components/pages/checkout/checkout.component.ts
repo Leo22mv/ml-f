b1a22ec43d8bcba02335808226067ea8327b6303
Kbza <g.leonel.m17@gmail.com>
2023-08-02 04:45:59 -0300

    0.95

--- src/app/components/pages/checkout/checkout.component.ts
+++ src/app/components/pages/checkout/checkout.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-checkout',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CheckoutComponent implements OnInit {
 
-  constructor() { }
+  total: number = 0;
+
+  constructor(public cartServ: CartService) { }
 
   ngOnInit(): void {
+    for (let product of this.cartServ.cart) {
+      this.total += product.price * product.quantity;
+    }
   }
 
 }
